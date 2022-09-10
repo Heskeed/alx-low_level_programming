d1eb7cb6e89fc3e6388d0b493bfab6f1e6430381
Heskeed <ndukwe_ejike@yahoo.com>
2022-09-09 20:08:36 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
+#include <stdio.h>
 #include <unistd.h>
-
-
 
 /**
 
@@ -15,9 +14,6 @@
 int main(void)
 
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
-
 }
