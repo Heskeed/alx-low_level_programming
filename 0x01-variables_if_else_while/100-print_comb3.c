c54fe9a2c60dd5d9ae25b52d102f43f922832406
Heskeed <ndukwe_ejike@yahoo.com>
2022-09-12 14:11:07 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,13 +10,13 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n <= 57; n++)
+	for (n = 38; n <= 47; n++)
 	{
-		for (m = 48; m <= 57; m++)
+		for (m = 38; m <= 47; m++)
 		{
 			putchar(n);
 			putchar(m);
-			if (n != 57 || m != 57)
+			if (n != 47 || m != 47)
 			{
 				putchar(',');
 				putchar(' ');
