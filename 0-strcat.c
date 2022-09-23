bb386750471ae083bd2d0ddc6ee00048cec279bd
Heskeed <ndukwe_ejike@yahoo.com>
2022-09-23 05:19:27 -0700

    Task 0

--- 0-strcat.c
+++ 0-strcat.c
@@ -6,11 +6,12 @@
  *@dest: String that will be appended
  *@src: String to be concatenated upon
  *Return: returns poiner to @dest
- /
+ */
 
 char *_strcat(char *dest, char *src)
 {
 	int index = 0, dest_len = 0;
+	
 	while (dest[index++])
 	dest_len++;
 	for (index = 0; src[index]; index++)
