34a11375cd015a108d766d3c6cc9c5565812d6c3
Heskeed <ndukwe_ejike@yahoo.com>
2022-10-05 14:12:14 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,12 +18,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	array = malloc(sizeof(char) * size);
-
 	if (array == NULL)
 		return (NULL);
-	
 	for (index = 0; index < size; index++)
 		array[index] = c;
-
 	return (array);
 }
