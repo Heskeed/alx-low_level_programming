d060c2425e7a2d92ef31bada2d14c2455f192a94
Heskeed <ndukwe_ejike@yahoo.com>
2022-10-13 07:26:57 -0700

    Task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -22,26 +22,19 @@ int main(int argc, char *argv[])
 	}
 	arg1 = atoi(argv[1]);
 	arg2 = atoi(argv[3]);
-
 	func = get_op_func(argv[2]);
-
 	if (!func)
 	{
 		printf("Error\n");
 			exit(99);
 	}
-
 	o = *argv[2];
-
 	if ((o == '/' || o == '%') && arg2 == 0)
 	{
 		printf("Error\n");
 			exit(100);
 	}
-
 	result = func(arg1, arg2);
-
 	printf("%d\n", result);
-
 	return (0);
 }
